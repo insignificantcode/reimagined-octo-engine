f26f0bc86f827532ad3e1d496181c05eb8315b33
insignificantcode <hunt.nicholas@gmail.com>
2020-03-18 13:32:24 -0700

    Update rdp_piv.sh

--- rdp_piv.sh
+++ rdp_piv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # simple script to log in RDP using smart card (PIV)
 
-xfreerdp -g 1920x1000 --plugin rdpsnd --plugin rdpdr --data scard:"SCM Microsystems, Inc. SCR331 SmartCard Reader" -- naswts3.ndc.nasa.gov
+xfreerdp -g 1920x1000 --plugin rdpsnd --plugin rdpdr --data scard:"SCM Microsystems, Inc. SCR331 SmartCard Reader" -- <hostname>
